ed3e23406d261cc5a3a46e6b87421c47a6f57ae8
Hannah Kwong <rc.kwong.hannah@leadps.org>
2016-11-17 18:42:51 +0000

    A message of multiples

--- multiples.py
+++ multiples.py
@@ -1,5 +1,10 @@
 print("For what number would you like multiples?") 
-num = int(raw_input())
+my_num = int(raw_input())
+num = 0 
+multiples = float(my_num * num)  
 
-while num < 1000
-	
+while multiples < 1000:
+	print(str(num) + " "  + "times" + " " +  str(my_num) + " "  + "equals" + " " + str(multiples)) 
+	num = num + 1 
+	multiples = float(my_num * num) 
+print("Those are all of the multiples of" + " " + str(my_num) + ", thank you for existing!")
